2ea16e115c5d5bce72c8ae2d031421e2badd813d
hiand <hiand@DESKTOP-AVSSGIQ>
2016-11-08 17:16:51 +0800

    Add project files.

---
+++ src/webapi/Bussiness/TimeMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi.Bussiness
+{
+    public class TimeMiddleware
+    {
+        private Stopwatch _stopwatch;
+        private RequestDelegate _next;
+        public TimeMiddleware(RequestDelegate next, Stopwatch stopwatch)
+        {
+            _next = next;
+            _stopwatch = stopwatch;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _stopwatch?.Start();
+            await _next(context);
+            await context.Response.WriteAsync($"<div>共耗时：{_stopwatch.ElapsedMilliseconds}</div>");
+        }
+    }
+}
---
+++ src/webapi/ClientApp/boot-server.js
@@ -0,0 +1,9 @@
+﻿module.exports = function (params) {
+    return new Promise(function (resolve, reject) {
+        var result = '<h1>Hello world!</h1>'
+            + '<p>Is gold user: ' + params.data.isGoldUser + '</p>'
+            + '<p>Number of cookies: ' + params.data.cookies.length + '</p>';
+
+        resolve({ html: result });
+    });
+};
